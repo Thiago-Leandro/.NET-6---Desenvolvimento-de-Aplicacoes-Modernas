477fd147fd739124394d7a2ac967a5f9bfce9b01
Thiago Leandro <tiago14789@live.com>
2024-02-28 22:22:36 -0300

    24. Deletar dado

--- Program.cs
+++ Program.cs
@@ -34,6 +34,11 @@ app.MapPut("/editproduct", (Product product) => {
     productSaved.Name = product.Name;
 });
 
+app.MapDelete("/deleteproduct/{code}", ([FromRoute] string code) => {
+    var productSaved = ProductRepository.GetBy(code);
+    ProductRepository.Remove(productSaved);
+});
+
 app.Run();
 
 public static class ProductRepository {
@@ -52,6 +57,10 @@ public static class ProductRepository {
 
     public static Product GetBy(string code) {
         return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static void Remove(Product product) {
+        Products.Remove(product);
     }
 }
 
